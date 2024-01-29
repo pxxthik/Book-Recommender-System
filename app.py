80bb93478b2671c30823e454968fc9098a370e2a
Pratheek Bedre <pratheekbedrejun20@gmail.com>
2024-01-29 09:14:00 +0530

    peaple also read feature

--- app.py
+++ app.py
@@ -9,10 +9,10 @@ popular = pickle.load(open("artifacts/popular.pkl", "rb"))
 
 def recommend_books(book_name):
     book_id = np.where(books_matrix.index == book_name)[0][0]
-    distances, suggestions = model.kneighbors(books_matrix.iloc[book_id, :].values.reshape(1,-1), n_neighbors=6)
+    distances, suggestions = model.kneighbors(books_matrix.iloc[book_id, :].values.reshape(1,-1), n_neighbors=books_matrix.shape[0])
     
     data = []
-    for i in range(1, len(suggestions[0])):
+    for i in range(1, 6):
         item = {}
         temp_df = books[books['title'] == books_matrix.index[suggestions[0]][i]]
         
@@ -24,7 +24,7 @@ def recommend_books(book_name):
         
         data.append(item)
     
-    return data
+    return data, distances
 
 st.title("Book Recommender System")
 
@@ -32,17 +32,28 @@ selected_book = st.selectbox(
     "Type or Select a Book", books_matrix.index)
 
 if st.button("Recommend"):
-    recommendations = recommend_books(selected_book)
+    recommendations, distances = recommend_books(selected_book)
+    distances = distances[0]
+
+    i = 1
 
     for book in recommendations:
+        chances = round((1-(distances[i]/distances[-1]))*100, 2)
+
+        chances += 20
+
         col1, col2 = st.columns(2)
         with col1:
             st.header(book['title'])
-            st.text(f"Author: {book['author']}")
             st.text(f"Year of Publication: {book['year']}")
             st.text(f"{book['num_ratings']}+ Reviews")
+            st.metric("People also read this book by", "", f"{chances}%")
         with col2:
             st.image(book['image_m'])
+            st.text(f"Author: {book['author']}")
+        
+        st.markdown("""---""")
+        i+= 1
 
 st.header("Popular Books")
 
